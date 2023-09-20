3efafea5c3b065b0d5b6d0ab431d1ee34dae69d8
mamckean12 <mamckean@gmail.com>
2023-09-20 16:43:40 -0400

    add comments to fetch code

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 // REMEMBER TO NPM START / BUILD*****
-
 import fetch from 'node-fetch';
 
 const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
 
-/**
- * @param name filter for color name
- * @param hex filter for color hex code
- * @param compName filter for complementary color name
- * @param compHex filter for complementary color hex code
- * @returns Promise
- */
-
-// async function to fetch colors
+// async function to fetch colors json and execute commands based on function parameters
 const fetchColors = async ({ name, hex, compName, compHex }) => {
 
-  // try fetching colors
+  // fetch colors json
   try {
     const response = await fetch(COLORS);
-    // throw error if network error occurs 
+
+    // throw error if network issue (rather than fetch issue) 
     if (!response.ok) {
       throw new Error(`Network response was not OK: ${error.message}`);
     }
-    // await json data via fetch
+
+    // await successful fetch then log complete message
     const colors = await response.json();
-    // log successful download and response
-    console.log("Download complete.", response);
-  // catch fetch error
+    console.log("Fetch complete.");
+
+    // conditionals that execute based on function parameters
+    // TBD
+
+  // catch and log fetch error
   } catch (error) {
-    // log fetch error
     console.error(`Download error: ${error.message}`);
   } 
-
 };
 
 export default fetchColors;
