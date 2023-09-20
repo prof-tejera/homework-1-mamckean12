141dc80380e39c942bcda81f7d9666d9048ff866
mamckean12 <mamckean@gmail.com>
2023-09-20 10:12:50 -0400

    add fetch function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// REMEMBER TO NPM START / BUILD*****
+
 import fetch from 'node-fetch';
-// Recommend using node-fetch for those familiar with JS fetch
 
 const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
 
@@ -10,9 +11,27 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @param compHex filter for complementary color hex code
  * @returns Promise
  */
-const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+
+// async function to fetch colors
+const fetchColors = async ({ name, hex, compName, compHex }) => {
+
+  // try fetching colors
+  try {
+    const response = await fetch(COLORS);
+    // throw error if network error occurs 
+    if (!response.ok) {
+      throw new Error(`Network response was not OK: ${error.message}`);
+    }
+    // await json data via fetch
+    const colors = await response.json();
+    // log successful download and response
+    console.log("Download complete.", response);
+  // catch fetch error
+  } catch (error) {
+    // log fetch error
+    console.error(`Download error: ${error.message}`);
+  } 
+
 };
 
-// Leave this here
 export default fetchColors;
